e2cb96c4b04a795ce2617886a8fbfdec55527927
diegogalvalisi <117120149+diegogalvalisi@users.noreply.github.com>
2023-05-24 10:53:55 -0600

    subir

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function () {
+    var now = new Date();
+    var day = now.getDate();
+    var month = now.getMonth();
+    var year = now.getFullYear() + 1;
+
+    var nextyear = '06/15/2023 17:00:00';
+
+    $('#timer').countdown({
+        date: nextyear, //TODO Date format: 07/27/2017 17:00:00
+        offset: +2,
+        day: 'Día',
+        days: 'Días'
+    }, function () {
+    });
+});
