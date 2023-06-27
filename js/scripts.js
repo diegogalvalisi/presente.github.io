c6251916f2b85cabf96281bc268f33a798ce488b
diegogalvalisi <117120149+diegogalvalisi@users.noreply.github.com>
2023-06-27 20:00:59 -0300

    asd

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(function () {
     var month = now.getMonth();
     var year = now.getFullYear() + 1;
 
-    var nextyear = '06/28/2023 17:00:00';
+    var nextyear = '06/9/2023 17:00:00';
 
     $('#timer').countdown({
         date: nextyear, //TODO Date format: 07/27/2017 17:00:00
